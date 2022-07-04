8f2c268606257da8aa551286ff969efd8f385c43
hwedi <mohamed.j.hwedi@gmail.com>
2022-07-04 17:32:26 +0200

    Add app logic

---
+++ app.js
@@ -0,0 +1,34 @@
+// DOM elements
+const quoteText = document.querySelector('#quote')
+const quoteAuthor = document.querySelector('#author');
+const newQuoteButton = document.querySelector('#new-quote')
+
+// Rendering fetched data
+function renderQuote(){
+   let randomNumber = Math.floor(Math.random() * 120);
+   quoteText.textContent =  apiQuotes[randomNumber].text;
+   quoteAuthor.textContent =  apiQuotes[randomNumber].author;
+}
+
+// API data
+const QUOTES_API = 'https://type.fit/api/quotes'
+let apiQuotes = []
+
+// API handling
+async function getQuotes(url) {
+    try {
+    const response = await fetch(url);
+    apiQuotes = await response.json();
+    renderQuote();
+    } catch(error) {
+        console.log(`Error: Can't retrieve data`)
+        console.log(`${error}`)
+    }
+}
+
+// Rendering new quote 
+newQuoteButton.addEventListener('click', () => {
+    renderQuote();
+})
+
+getQuotes(QUOTES_API);
